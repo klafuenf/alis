939ad802cf56b745231e41861b0d9f2b435d7e4e
Peter Kaiser <pk@pkaiser.de>
2020-10-08 20:37:38 +0200

    patch path

--- alis-recovery.sh
+++ alis-recovery.sh
@@ -18,7 +18,7 @@ set -e
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# This script is hosted at https://github.com/picodotdev/alis. For new features,
+# This script is hosted at https://github.com/klafuenf/alis. For new features,
 # improvements and bugs fill an issue in GitHub or make a pull request.
 # Pull Request are welcome!
 #
@@ -39,7 +39,7 @@ set -e
 
 # Usage:
 # # loadkeys es
-# # curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash, or with URL shortener curl -sL https://bit.ly/2F3CATp | bash
+# # curl https://raw.githubusercontent.com/klafuenf/alis/master/download.sh | bash, or with URL shortener curl -sL https://bit.ly/2F3CATp | bash
 # # vim alis-recovery.conf
 # # ./alis-recovery.sh
 
--- site/install.sh
+++ site/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-git clone -b master git@github.com:picodotdev/alis.git
+git clone -b master git@github.com:klafuenf/alis.git
 cd alis
-git config --local user.email "pico.dev@gmail.com"
-git config --local user.name "pico.dev"
+git config --local user.email "dev@klafuenf.eu"
+git config --local user.name "dev"
 
-git clone -b gh-pages git@github.com:picodotdev/alis.git deploy/
+git clone -b gh-pages git@github.com:klafuenf/alis.git deploy/
 cd deploy
-git config --local user.email "pico.dev@gmail.com"
-git config --local user.name "pico.dev"
+git config --local user.email "dev@klafuenf.eu"
+git config --local user.name "dev"
 
